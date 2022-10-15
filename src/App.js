c9f1112805b6b49ffd8abf2ded758360b6e0361e
weeeeeeee <95114619+meA-sonWat@users.noreply.github.com>
2022-10-15 14:10:25 +0800

    undone

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+        <div className='blur-背景' style={{top: '-18%', right: '0'}}></div>
+        <div className='blur-背景' style={{top: '36%', left: '-8rem'}}></div>
+    </div>
+  );
+}
+
+export default App;
