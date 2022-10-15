005a614512cb59f0f5dbaa1abbe5f807d901f9b4
weeeeeeee <95114619+meA-sonWat@users.noreply.github.com>
2022-10-15 15:32:07 +0800

    undone

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css"
+import Home頁面 from "./pages/home/Home頁面";
+
 function App() {
   return (
     <div className="App">
         <div className='blur-背景' style={{top: '-18%', right: '0'}}></div>
         <div className='blur-背景' style={{top: '36%', left: '-8rem'}}></div>
+        <Home頁面></Home頁面>
     </div>
   );
 }
---
+++ src/pages/home/Home頁面.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home頁面 = () => {
+  return (
+    <div className='Home-頁面'>
+        <div className="profileSide-頭像">Profiles</div>
+        <div className="postSide-打卡">Posts</div>
+        <div className="rightSide-右邊">Rightside</div>
+    </div>
+  )
+}
+
+export default Home頁面
