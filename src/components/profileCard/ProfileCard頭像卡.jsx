2209d3c726d7593c07cb2961de53c3ae70246ef9
weeeeeeee <95114619+meA-sonWat@users.noreply.github.com>
2022-10-17 22:19:14 +0800

    lebih laaaaaa

--- src/components/profileCard/ProfileCard頭像卡.jsx
+++ src/components/profileCard/ProfileCard頭像卡.jsx
@@ -10,6 +10,32 @@ const ProfileCard頭像卡 = () => {
         <img src={Cover覆蓋} alt=''/>
         <img src={Profile頭像} alt=''/>
       </div>
+
+      <div className="ProfileName-頭像名字">
+        <span>WinsonOTP</span>
+        <span>Senior Bot Designer</span>
+      </div>
+
+      <div className="FollowStatus-跟隨狀態">
+        <hr/>
+        <div>
+          <div className="Follow-跟隨">
+            <span>614</span>
+            <span>Followers</span>
+          </div>
+          <div className="vl"></div>
+          <div className="Follow-跟隨">
+            <span>1</span>
+            <span>Following</span>
+          </div>
+        </div>
+        <hr/>
+      </div>
+
+      <span>
+        My Profile
+      </span>
+
     </div>
   )
 }
--- src/components/profileSide/ProfileSide頭像.jsx
+++ src/components/profileSide/ProfileSide頭像.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LogoSearch標志搜尋 from '../logoSearch/LogoSearch標志搜尋'
 import ProfileCard頭像卡 from '../profileCard/ProfileCard頭像卡'
 
+import './ProfileSide頭像.css'
 const ProfileSide頭像 = () => {
   return (
     <div className="ProfileSide-頭像">
