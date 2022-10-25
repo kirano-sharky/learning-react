b39627e4e379f6ab3b4a70f88f442aa0b76ba45b
weeeeeeee <95114619+meA-sonWat@users.noreply.github.com>
2022-10-25 21:26:15 +0800

    lebih laaaaa

--- src/components/rightSide/RightSide右邊.jsx
+++ src/components/rightSide/RightSide右邊.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './RightSide右邊.css'
 import Home頁面 from '../../img/home.png'
-import Noti通知 from '../../img/noti.png'
-import Message訊息 from '../../img/message.png'
+import { UilBell } from '@iconscout/react-unicons'
+import { UilMessage } from '@iconscout/react-unicons'
 import { UilSetting } from '@iconscout/react-unicons'
+import { UilChartBar } from '@iconscout/react-unicons'
 import Htag from '../../img/htag.png'
 import TrendCard推薦卡 from '../trendCard/TrendCard推薦卡'
 
@@ -11,11 +12,21 @@ const RightSide右邊 = () => {
   return (
     <div className="RightSide-右邊">
         <div className="icon圖標">
+          <div className="home icon-option">
             <img src={Home頁面}/>
+          </div>
+          <div className="icon-option">
             <UilSetting/>
-            <img src={Noti通知}/>
-            <img src={Message訊息}/>
-            <img src={Htag}/>
+          </div>
+          <div className="icon-option">
+            <UilBell/>
+          </div>
+          <div className="icon-option">
+            <UilMessage/>
+          </div>
+          <div className="icon-option">
+            <UilChartBar/>
+          </div>
         </div>
 
         <TrendCard推薦卡/>
